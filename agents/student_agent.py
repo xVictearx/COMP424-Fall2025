04c0b6922b35d2de26b767ea883fe96db08d382a
Victor Xie <xvictearx@gmail.com>
2025-11-30 00:56:00 -0500

    after getting together, we fixed some values, all thats left is to retest each board in autoplay

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -12,71 +12,95 @@ EVAL_WEIGHTS = {
     # board[1,1] == 3
     "big_x": {
         "score_diff":   1,   # material still matters
-        "gap_offender": 20,   # small reward for blobs
+        "gap_offender": 35,   # small reward for blobs
         "gap_defender": 30,   # mild penalty for bad jumps leaving holes
-        "move_bonus":   30,   # jumps slightly punished (your move_bonus is 0 or -5)
+        "move_bonus":   40,   # jumps slightly punished (your move_bonus is 0 or -5)
         #"board_bonus":  0.0,   # no special positional mask
-        "mobility": 50,   # open so mobility important
+        "mobility": 5,   # open so mobility important
+        "obst_bonus":  0,
+        "obstacle": []
     },
 
     # board[2,3] == 3 and board[1,3] != 3
     "plus1": {
-        "score_diff":   1.0,
-        "gap_offender": 10,   # blobs around cross arms are nice
-        "gap_defender": 20,   # avoid leaving big 1-holes near the plus
-        "move_bonus":   30,   # jumps are somewhat risky
+        "score_diff":   1,
+        "gap_offender": 35,   # blobs around cross arms are nice
+        "gap_defender": 30,   # avoid leaving big 1-holes near the plus
+        "move_bonus":   40,   # jumps are somewhat risky
         #"board_bonus":  0.8,   # if you later add a plus1 bonus grid
-        "mobility": 40,   # open so mobility important
+        "mobility": 5,   # open so mobility important
+        "obst_bonus":  0,
+        "obstacle": []
     },
 
     # board[1,3] == 3
     "plus2": {
-        "score_diff":   1.0,
-        "gap_offender": 10,   # like plus1 but a bit stronger clustering
-        "gap_defender": 30,
-        "move_bonus":   20,
+        "score_diff":   1,
+        "gap_offender": 30,   # like plus1 but a bit stronger clustering
+        "gap_defender": 35,
+        "move_bonus":   40,
         #"board_bonus":  1.0,
-        "mobility": 20,   # open so mobility important
+        "mobility": 5,   # open so mobility important
+        "obst_bonus":  0,
+        "obstacle": []
     },
 
     # board[2,2] == 3
     "point4": {
         "score_diff":   1,
-        "gap_offender": 10,   # islands-ish: safe blobs good
-        "gap_defender": 30,   # really punish dangerous gaps
-        "move_bonus":   20,
+        "gap_offender": 40,   # islands-ish: safe blobs good
+        "gap_defender": 35,   # really punish dangerous gaps
+        "move_bonus":   60,
         #"board_bonus":  1.2,   # central-ish control good
-        "mobility": 40,   # open so mobility important
+        "mobility": 5,   # open so mobility important
+        "obst_bonus":  10,
+        "obstacle": []
     },
 
     # board[1,2] == 3
     "circle": {
         "score_diff":   1,
-        "gap_offender": 10,   # ring control
-        "gap_defender": 20,
-        "move_bonus":   30,
+        "gap_offender": 45,   # ring control
+        "gap_defender": 15,
+        "move_bonus":   10,
         #"board_bonus":  1.0,   # if you have a ring bonus mask later
-        "mobility": 20,   # open so mobility important
+        "mobility": 5,   # open so mobility important
+        "obst_bonus":  0,
+        "obstacle": []
+        
     },
 
     # board[0,3] == 3
     "wall": {
         "score_diff":   1,
-        "gap_offender": 30,   # big safe blobs behind wall are great
-        "gap_defender": 10,   # strongly avoid 1-hole disasters
-        "move_bonus":   20,   # jumps more dangerous here
+        "gap_offender": 5,   # big safe blobs behind wall are great
+        "gap_defender": 20,   # strongly avoid 1-hole disasters
+        "move_bonus":   0,   # jumps more dangerous here
         #"board_bonus":  2.0,   # chokepoints near the wall are super valuable
-        "mobility": 0,   # not open so mobility does not matter
+        "mobility": 100,   
+        "obst_bonus":  10,
+        "obstacle": [
+                        (0,2),      (0,4),
+                        (1,2),      (1,4),
+          (2,0), (2,1), (2,2),      (2,4), (2,5), (2,6),
+                              
+          (3,0), (3,1), (3,2),      (3,4), (3,5), (3,6),
+                        (4,2),      (4,4),
+                        (5,2),      (5,4),
+                        (6,2),      (6,4)
+                    ]
     },
 
     # board[0,2] == 3
     "watch_sides": {
         "score_diff":   1,
-        "gap_offender": 10,   # side clusters good
-        "gap_defender": 20,
-        "move_bonus":   10,
+        "gap_offender": 40,   # side clusters good
+        "gap_defender": 40,
+        "move_bonus":   60,
         #"board_bonus":  1.5,   # strongly prefer good side squares
-        "mobility": 50,   # open so mobility important
+        "mobility": 5,   # open so mobility important
+        "obst_bonus":  0,
+        "obstacle": []
     },
 
     # else: empty / no obstacles
@@ -84,9 +108,11 @@ EVAL_WEIGHTS = {
         "score_diff":   1,
         "gap_offender": 10,   # moderate value for clustering
         "gap_defender": 20,   # avoid terrible gaps but not overkill
-        "move_bonus":   10,   # your original: jumps punished by full move_bonus
+        "move_bonus":   30,   # your original: jumps punished by full move_bonus
         #"board_bonus":  0.5,   # mild center-ish preference if you use it
-        "mobility": 50,   # open so mobility important
+        "mobility": 5,   # open so mobility important
+        "obst_bonus":  0,
+        "obstacle": []
     },
 }
 
@@ -96,10 +122,12 @@ class StudentAgent(Agent):
   A class for your implementation. Feel free to use this class to
   add any helper functionalities needed for your agent.
   """
-  boards = []
+ 
   def __init__(self):
     super(StudentAgent, self).__init__()
     self.name = "StudentAgent"
+    self.turns = 0
+    self.is_difficult = False
 
   def step(self, chess_board, you, opp):
     """
@@ -109,7 +137,7 @@ class StudentAgent(Agent):
 
     Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
     """
-
+    
     start_time = time.time()
     
     legal_moves = get_valid_moves(chess_board, you)
@@ -117,8 +145,8 @@ class StudentAgent(Agent):
     if not legal_moves:
       return None  # No valid moves available, pass turn
     
-    move_set = self.split_moves(legal_moves)
-
+    
+    
     a = float('-inf')
     b =  float('inf')
     best_score = float('-inf')
@@ -128,6 +156,20 @@ class StudentAgent(Agent):
     at_row, at_col = 0, 0
     dest_row, dest_col = 0, 0
     self.boards = []
+    boardt_type = self.get_board_type(chess_board)
+    move_set = []
+    
+    if self.turns < 3 and (boardt_type == "wall" or boardt_type == "watch_sides" or boardt_type == "circle"):
+      # if boardt_type == "wall" :
+      #   self.turns = 2
+      self.is_difficult = True
+      move_set = self.split_moves(legal_moves)
+      
+      move_set = [move_set[0]]
+    else:
+      move_set = self.split_moves(legal_moves)
+
+      # print(move_set)
     for moves in move_set:
       for move in moves:
         
@@ -149,6 +191,8 @@ class StudentAgent(Agent):
         if val > best_score:
           best_score = val
           best_move = move  
+
+    self.turns += 1
     print(best_score)
     # for move in legal_moves:
       
@@ -260,6 +304,8 @@ class StudentAgent(Agent):
     return b
   
   def evalMove(self, board, move, you, opp, i, og_move):
+    btype = self.get_board_type(board)
+    
     finishing_move = self.last_move_win(board, you, opp)
     player_count = np.count_nonzero(board == you)
     opp_count = np.count_nonzero(board == opp)
@@ -274,12 +320,16 @@ class StudentAgent(Agent):
     # gap_penalty = 0
     edge_bonus =  20 * 0 #* self.edge_bonus(board, you) 
     center_bonus =  2 * 0 #* self.center_bonus(board, you) 
+    # if self.get_board_type(board) == "wall":
+    #   center_bonus = 200 * self.center_bonus(board, you)
     move_bonus =  self.move_bonus(move, you) 
     #safe_territory_bonus = 10 * self.far_from_opp(board, move, you, opp)
 
-    btype = self.get_board_type(board)
-    w = EVAL_WEIGHTS[btype]
+    near_wall_bonus = self.near_wall_bonus(board, you)
 
+   
+    w = EVAL_WEIGHTS[btype]
+   
     total_score = (w["score_diff"] * score_diff + 
                    w["gap_offender"] * gap_offender + 
                    w["gap_defender"] * gap_defender + 
@@ -288,7 +338,9 @@ class StudentAgent(Agent):
                    edge_bonus +  
                    finishing_move + 
                    center_bonus + 
-                   w["move_bonus"] * move_bonus 
+                   w["move_bonus"] * move_bonus +
+                   w["obst_bonus"] * near_wall_bonus
+
                    # 0 * board_bonus 
                    #safe_territory_bonus 
                    )
@@ -312,7 +364,9 @@ class StudentAgent(Agent):
   def mobility(self, board, you, opp):
     opp_moves = len(get_valid_moves(board, opp))
     you_moves = len(get_valid_moves(board, you))
-    return -opp_moves
+    if self.get_board_type(board) == "wall":
+      return - opp_moves
+    return you_moves-opp_moves
   
   def gap_defender(self, board, move, you, opp):
     n = board.shape[0] 
@@ -334,23 +388,15 @@ class StudentAgent(Agent):
               opp_pos = at_row + row_diff ,  at_col + col_diff
             
               if 0 <= opp_pos[0] < n and 0 <= opp_pos[1] < n:
-                if board[opp_pos[0], opp_pos[1]] == you:
-                  opp_close_by += 1
+                if self.get_board_type(board) == "wall":
+                  if board[opp_pos[0], opp_pos[1]] == you or board[opp_pos[0], opp_pos[1]] == 3:
+                    opp_close_by += 1
+                else:
+                  if board[opp_pos[0], opp_pos[1]] == you :
+                    opp_close_by += 1
             if opp_close_by >= 5:
               gap_penalty -= opp_close_by
-      # for row in range(n):
-      #   for col in range(n):
-      #     if board[row][col] == 0:
-      #       opp_close_by = 0
-
-      #       for row_diff, col_diff in dir_coords:
-      #         opp_pos = row + row_diff ,  col + col_diff
-            
-      #         if 0 <= opp_pos[0] < n and 0 <= opp_pos[1] < n:
-      #           if board[opp_pos[0], opp_pos[1]] == you:
-      #             opp_close_by += 1
-      #       if opp_close_by >= 5:
-      #         gap_penalty -= opp_close_by
+  
             
 
     return gap_penalty
@@ -376,8 +422,12 @@ class StudentAgent(Agent):
             opp_pos = row + row_diff ,  col + col_diff
            
             if 0 <= opp_pos[0] < n and 0 <= opp_pos[1] < n:
-              if board[opp_pos[0], opp_pos[1]] == you:
-                opp_close_by += 1
+              if self.get_board_type(board) == "wall":
+                if board[opp_pos[0], opp_pos[1]] == you or board[opp_pos[0], opp_pos[1]] == 3:
+                  opp_close_by += 1
+              else:
+                if board[opp_pos[0], opp_pos[1]] == you :
+                  opp_close_by += 1
           
           gap_penalty += opp_close_by 
 
@@ -428,13 +478,14 @@ class StudentAgent(Agent):
     return edge_bonus
 
   def center_bonus(self, board, you):
-    return 0
+    
     n = board.shape[0] 
     center = [
       (n//2 - 1, n//2 -1), (n//2 - 1, n//2), (n//2 - 1, n//2 + 1), 
       (n//2, n//2 - 1),        (n//2, n//2), (n//2,n//2 + 1),
       (n//2 + 1, n//2 -1), (n//2 + 1, n//2), (n//2 + 1, n//2 + 1)
     ]
+    center = [(n//2, n//2)]
     center_bonus = sum(1 for (i, j) in center if board[i, j] == you)
     return center_bonus
   
@@ -455,11 +506,18 @@ class StudentAgent(Agent):
     for move in moves:
       at_row, at_col = move.get_src()
       dest_row, dest_col = move.get_dest()
+      if self.is_difficult:
+        if abs(dest_row - at_row) == 2 or abs(dest_col - at_col) == 2:
+          jump_moves.append(move)
 
-      if dest_row - at_row == 2 or dest_col - at_col == 2:
-        jump_moves.append(move)
-      else:
-        duplicate_moves.append(move)
+        else:
+          duplicate_moves.append(move)
+
+      else: 
+        if dest_row - at_row == 2 or dest_col - at_col == 2:
+          jump_moves.append(move)
+        else:
+          duplicate_moves.append(move)
 
     move_set = [duplicate_moves, jump_moves]
     return move_set
@@ -502,7 +560,7 @@ class StudentAgent(Agent):
     if (board[2,3] == 3 and board[1, 3] != 3):
       # Board is plus1
       return "plus1"
-    if (board[1, 3] == 3):
+    if (board[1, 3] == 3 and board[3,0] != 3):
       # board is plus2
       return "plus2"
     if (board[2, 2] == 3):
@@ -533,100 +591,10 @@ class StudentAgent(Agent):
     #    [ 0,  0,  10,  0,  10,  0,  0]
     #  ]
         
+  def near_wall_bonus(self, board, you):
+    board_type = self.get_board_type(board)
+    w = EVAL_WEIGHTS[board_type]      
+    near_wall_bonus = sum(1 for (i, j) in w["obstacle"] if board[i, j] == you)
 
-  # give higher priority to moves that can't be attacked by opponent
-  def far_from_opp(self, board, move, you, opp):
-    n = board.shape[0] 
-    for row in range(n):
-      safe_count = 0
-    total_pieces = 0
-
-    for r in range(n):
-        for c in range(n):
-            if board[r][c] != you:
-                continue
-
-            total_pieces += 1
-            threatened = False
-
-            # Look for any opponent piece within Chebyshev distance <= 3
-            for dr in range(-3, 4):
-                if threatened:
-                    break
-                for dc in range(-3, 4):
-                    if dr == 0 and dc == 0:
-                        continue
-                    nr = r + dr
-                    nc = c + dc
-                    if 0 <= nr < n and 0 <= nc < n:
-                        if board[nr][nc] == opp:
-                            threatened = True
-                            break
-
-            if not threatened:
-                safe_count += 1
-
-    if total_pieces == 0:
-        return 0.0
-
-    # Fraction of your pieces that are "safe"
-    return (int) (safe_count / total_pieces)
-    
-
-  def evalMove2(self, board, move, you, opp, i):
-    if check_endgame(board):
-      return 5000
-    player_count = np.count_nonzero(board == you)
-    opp_count = np.count_nonzero(board == opp)
-    score_diff = player_count - opp_count
-    at_row, at_col = move.get_src()
-    dest_row, dest_col = move.get_dest()
-    jmp_move = 0
-    n = board.shape[0] 
-    zeros = n^2 - np.count_nonzero(board)
-    
-    
-
-    # mvs = get_valid_moves(board, you)
-    # for mv in mvs :
-    #   at_row2, at_col2 = mv.get_src()
-    #   dest_row2, dest_col2 = mv.get_dest()
-    #   if abs(dest_row2 - at_row2) == 2 or abs(dest_col2 - at_col2) == 2:
-    #     jmp_moves += 1
-   
-
-    # corner control bonus
-   
-   
-
-    if abs(dest_row - at_row) == 2 or abs(dest_col - at_col) == 2:
-      zeros = n^2 - np.count_nonzero(board)
-      print(player_count + opp_count == np.count_nonzero(board))
-      if zeros >= (n**2) / 4:
-        jmp_move = - 50
-    # # for x in range(n):
-    # #   for y in range(n):
-    # #     if board[x][y] == 0:
-    # #       i = 0
-    # #       if  x - 1 >= 0:
-
-
-    # #       adj = 
-    # #   return 0
-    corners = [(0, 0), (0, n - 1), (n - 1, 0), (n - 1, n - 1)]
-    
-    corner_bonus = sum(1 for (i, j) in corners if board[i, j] == you) * 5
-    # edges = []
-    # for x in range(n):
-    #   edges.append((0,x))
-    #   edges.append((x,0))
-    #   edges.append((n-1,x))
-    #   edges.append((x,n-1))
-    # edge_bonus = sum(1 for (i, j) in edges if board[i, j] == you)
-    # penalize opponent mobility
-    opp_moves = len(get_valid_moves(board, opp))
-    mobility_penalty = -opp_moves
-
-    return score_diff + jmp_move + mobility_penalty
-
+    return near_wall_bonus
         
